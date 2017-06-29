5f3a4db4c9c3dbce49acd5976057ff9a06e8b689
Michael McAnulty <michael.mcanulty88@gmail.com>
2017-06-28 22:06:09 -0700

    Add files via upload

---
+++ lib/year-view.d.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { AfterContentInit, EventEmitter } from '@angular/core';
+import { MdCalendarCell } from './calendar-body';
+import { MdCalendar } from './calendar';
+import { TimepickerAttrs } from './timepicker-attrs';
+import { DateAdapter } from './native-date-module/index';
+import { MdDateFormats } from '@angular/material';
+/**
+ * An internal component used to display a single year in the datetimepicker.
+ * @docs-private
+ */
+export declare class MdYearView<D> implements AfterContentInit {
+    private _calendar;
+    _dateAdapter: DateAdapter<D>;
+    private _dateFormats;
+    /** The date to display in this year view (everything other than the year is ignored). */
+    activeDate: D;
+    private _activeDate;
+    /** The currently selected date. */
+    selected: D;
+    private _selected;
+    /** A function used to filter which dates are selectable. */
+    dateFilter: (date: D) => boolean;
+    /** Emits when a new month is selected. */
+    selectedChange: EventEmitter<D>;
+    /** Grid of calendar cells representing the months of the year. */
+    _months: MdCalendarCell[][];
+    /** The label for this year (e.g. "2017"). */
+    _yearLabel: string;
+    /** The month in this year that today falls on. Null if today is in a different year. */
+    _todayMonth: number;
+    timepickerAttrs: TimepickerAttrs;
+    private _timepickerAttrs;
+    /**
+     * The month in this year that the selected Date falls on.
+     * Null if the selected Date is in a different year.
+     */
+    _selectedMonth: number;
+    constructor(_calendar: MdCalendar<D>, _dateAdapter: DateAdapter<D>, _dateFormats: MdDateFormats);
+    ngAfterContentInit(): void;
+    /** Handles when a new month is selected. */
+    _monthSelected(month: number): void;
+    /** Initializes this month view. */
+    private _init();
+    /**
+     * Gets the month in this year that the given Date falls on.
+     * Returns null if the given Date is in another year.
+     */
+    private _getMonthInCurrentYear(date);
+    /** Creates an MdCalendarCell for the given month. */
+    private _createCellForMonth(month, monthName);
+    /** Whether the given month is enabled. */
+    private _isMonthEnabled(month);
+}
